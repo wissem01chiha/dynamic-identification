00dc27c7357b84b6c6ff7d56e9ce299428dec637
wissem01chiha <chihawissem08@gmail.com>
2024-08-11 19:32:37 +0100

    final version & deployement & release

--- setup.py
+++ setup.py
@@ -5,17 +5,19 @@ setuptools.setup(
     author="Wissem CHIHA",
     author_email="chihawissem08@gmail.com",
     packages=setuptools.find_packages(),
+    package_data={'pyDynaMapp': ['data/*.csv', 'robot/*.urdf', 'robot/*.yml']},
     description="pyDynaMapp package",
     url="https://github.com/wissem01chiha/dynamic-identification",
     python_requires='>=3.6',
     include_package_data=True,
     install_requires=[
         "numpy==1.26.4",
-        "pinocchio=2.7.1",
-        "scipy=1.14.0",
+        "numba==0.59.0",
+        "pinocchio==2.7.1",
+        "scipy==1.14.0",
         "setuptools==68.1.2",
-        "nlopt=2.7.1",
-        "pandas=2.2.2",
-        "matplotlib=3.8.4"
+        "nlopt==2.7.1",
+        "pandas==2.2.2",
+        "matplotlib==3.8.4"
     ],
 )
